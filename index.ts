3512bd34d469e985b3f2694c1710a41ebc8dac1c
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2021-02-16 02:34:14 -0300

    Added user control

--- index.ts
+++ index.ts
@@ -1,22 +1,44 @@
 import express from 'express';
+import session from 'express-session';
+import mdbSession from 'connect-mongodb-session';
 import morgan from 'morgan';
 
 import config from '@configs/server';
 import handleError from '@helpers/handleError';
 import '@configs/database';
 
-import Users from '@routes/users';
+import Users from '@routes/Users';
 
 console.clear();
 
 const app = express();
 const PORT = config.server.port;
 
+const MongoDbSession = mdbSession(session);
+
+const store = new MongoDbSession({
+  uri: process.env.DB_URI || '',
+  collection: 'sessions',
+});
+
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(handleError);
+app.use(
+  session({
+    name: 'hyanid',
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    secret: process.env.KEY!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 31,
+    },
+    store,
+  }),
+);
 
-app.get('/', (req, res) => res.redirect('https://hyan.dev'));
 app.use('/api/v1/users', Users);
 
 app.listen(PORT, () => {
